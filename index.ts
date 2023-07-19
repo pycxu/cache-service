a9d1e11147eb1f8d2e8f76dbd3d813ea9ae356c4
pycxu <pyc.xuu@gmail.com>
2023-07-19 11:53:11 +1000

    Add generic type and expiry time

--- index.ts
+++ index.ts
@@ -1,21 +1,43 @@
 import './style.css';
 import { of, Observable, shareReplay } from 'rxjs';
 
-class CacheService {
-  private readonly cacheMap = new Map<string, Observable<any>>();
-  private readonly CACHE_SIZE = 1;
+interface ICache<T> {
+  value$: Observable<T>;
+  expiry: number;
+}
 
-  public cachify<T>(
-    cacheKey: string,
-    cacheValue$: Observable<T>
-  ): Observable<T> {
-    if (!this.cacheMap.has(cacheKey)) {
-      this.cacheMap.set(
-        cacheKey,
-        cacheValue$.pipe(shareReplay(this.CACHE_SIZE))
-      );
+class CacheService {
+  private readonly cacheMap = new Map<string, ICache<unknown>>();
+  private readonly CACHE_SIZE = 1;
+  private readonly CACHE_EXPIRY_INTERVAL = 10 * 60 * 1000; // 10 minutes
+
+  public cachify<T>(key: string, value$: Observable<T>): Observable<T> {
+    if (!this.cached(key)) this.set<T>(key, value$);
+
+    return this.get<T>(key);
+  }
+
+  private cached(key: string): boolean {
+    if (!this.cacheMap.has(key)) return false;
+
+    if (this.cacheMap.get(key).expiry < Date.now()) {
+      this.cacheMap.delete(key);
+      return false;
     }
-    return this.cacheMap.get(cacheKey);
+
+    return true;
+  }
+
+  private set<T>(key: string, value$: Observable<T>) {
+    const value: ICache<T> = {
+      value$: value$.pipe(shareReplay(this.CACHE_SIZE)),
+      expiry: Date.now() + this.CACHE_EXPIRY_INTERVAL,
+    };
+    this.cacheMap.set(key, value);
+  }
+
+  private get<T>(key: string): Observable<T> {
+    return this.cacheMap.get(key).value$ as Observable<T>;
   }
 }
 
